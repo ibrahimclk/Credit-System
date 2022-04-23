21bdfd538df85c33118eb42acbbf39d23eead73c
ibrahimclk <ibrhmclk.95@gmail.com>
2022-04-23 17:33:11 +0300

    Dtos edited

--- src/main/java/com/paycore/CreditSystem/model/DTO/ApplicationDTO.java
+++ src/main/java/com/paycore/CreditSystem/model/DTO/ApplicationDTO.java
@@ -1,9 +1,13 @@
 package com.paycore.CreditSystem.model.DTO;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApplicationDTO {
     private Integer credit_limit;
     private String credit_result;
--- src/main/java/com/paycore/CreditSystem/model/DTO/CustomerDTO.java
+++ src/main/java/com/paycore/CreditSystem/model/DTO/CustomerDTO.java
@@ -1,9 +1,14 @@
 package com.paycore.CreditSystem.model.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CustomerDTO {
 
     private String identityno;

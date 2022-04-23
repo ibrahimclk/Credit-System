06fa42f1f4df8bf980357d1010155c2c6e4e6d21
ibrahimclk <ibrhmclk.95@gmail.com>
2022-04-23 17:29:58 +0300

    Mapper edited

--- src/main/java/com/paycore/CreditSystem/model/mapper/ApplicationMapper.java
+++ src/main/java/com/paycore/CreditSystem/model/mapper/ApplicationMapper.java
@@ -5,12 +5,16 @@ import com.paycore.CreditSystem.model.entity.Application;
 import com.paycore.CreditSystem.model.entity.Customer;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface ApplicationMapper {
 
     ApplicationDTO toDto(Application entity);
 
     Application toEntity(Customer dto);
+
+    List<ApplicationDTO> toDtos(List<Application> applications);
 
 
 }
